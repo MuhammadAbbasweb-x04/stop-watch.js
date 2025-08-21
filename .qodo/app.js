57d9de63c0dcba8551a99530034e081796fe6510
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-08-21 16:51:09 +0500

    added

--- .qodo/app.js
+++ .qodo/app.js
@@ -3,16 +3,46 @@ let minute = document.querySelector("#minute");
 let second = document.querySelector("#second");
 
 
-function secfnc(){
-const sec += 0;
+function satrtfnc(){
+let sec = 0;
+let min = 0;
+let hur = 0;
 setInterval (function(){
-if(sec < 9){
-second.innerHTML = sec + "0" 
+    sec++
+if(sec === 60){
+// sec = 0;
+min++
+
 }
-
-
+if(min === 60){
+// min = 0;
+hur++
+}
+if(sec < 10){
+second.innerHTML = "0" +  sec;
+} else{
+    second.innerHTML = sec;
+}
 },1000)
-
-
+ 
+if(min < 10){
+minute.innerHTML = "0" +  min;
+} else{
+minute.innerHTML = min;
+}
+if(hur < 10){
+hour.innerHTML = "0" +  hur;
+} else{
+    hour.innerHTML = hur;
+}
 
 }
+
+
+// function stopfnc(){
+// clearInterval (function (){
+
+// })
+// }
+
+
