61d6e43d64e4013dfce8ff7b0b6a7a22d4b4984e
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-08-21 13:14:22 +0500

    added

---
+++ .qodo/app.js
@@ -0,0 +1,18 @@
+let hour = document.querySelector("#hour");
+let minute = document.querySelector("#minute");
+let second = document.querySelector("#second");
+
+
+function secfnc(){
+const sec += 0;
+setInterval (function(){
+if(sec < 9){
+second.innerHTML = sec + "0" 
+}
+
+
+},1000)
+
+
+
+}
